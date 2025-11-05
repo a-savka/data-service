49480983dab30b736452b75d34cd909107893ce2
Andrey Savka <a_sawka@yahoo.com>
2025-11-05 23:31:57 +0300

    implement students gate controller

---
+++ gate/src/main/java/ru/savka/gate/controller/StudentGateController.java
@@ -0,0 +1,38 @@
+package ru.savka.gate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.savka.gate.api.StudentGateApi;
+import ru.savka.gate.client.api.StudentDataApi;
+import ru.savka.gate.client.model.StudentDataCreateRequest;
+import ru.savka.gate.client.model.StudentDataResponse;
+import ru.savka.gate.model.StudentGateCreateRequest;
+import ru.savka.gate.model.StudentGateResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentGateController implements StudentGateApi {
+
+    private final StudentDataApi studentsFeignClient;
+
+    /**
+     * Проксирует создание студента во внутренний DATA-SERVICE.
+     * Получает запрос от клиента, преобразует модель и перенаправляет.
+     */
+    @Override
+    public ResponseEntity<StudentGateResponse> createStudent(StudentGateCreateRequest request) {
+        StudentDataCreateRequest dataRequest = new StudentDataCreateRequest();
+        dataRequest.setFullName(request.getFullName());
+        dataRequest.setPassport(request.getPassport());
+
+        StudentDataResponse dataResponse = studentsFeignClient.createStudentDataInData(dataRequest);
+
+        StudentGateResponse gateResponse = new StudentGateResponse();
+        gateResponse.setId(dataResponse.getId());
+        gateResponse.setFullName(dataResponse.getFullName());
+        gateResponse.setPassport(dataResponse.getPassport());
+
+        return ResponseEntity.status(201).body(gateResponse);
+    }
+}
